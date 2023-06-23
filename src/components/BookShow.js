4b74f33f21b98feb695b557bd20fb4084fc8227d
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-06-23 07:12:47 -0500

    added changing and deletion of content,

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,5 +1,25 @@
-function BookShow() {
-  return <div>BookShow</div>;
+import { useState } from "react";
+
+function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("New title", title);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
 
-export default BookShow;
+export default BookEdit;
